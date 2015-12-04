c50ec06d6168706575a5a407d2204cfa0c802f31
Brendan Butler <b.butler@kainos.com>
2015-12-04 12:12:48 +0000

    Add person page fields and views

--- apps/gro/fields/person.js
+++ apps/gro/fields/person.js
@@ -4,24 +4,5 @@ module.exports = {
   'person-text': {
     validate: ['required'],
     label: 'fields.person-text.label'
-  },
-  'yes-no-radio': {
-    validate: ['required'],
-    className: ['inline', 'form-group'],
-    options: [{
-      value: 'Yes',
-      label: 'Yes'
-    }, {
-      value: 'No',
-      label: 'No'
-    }]
-  },
-  'example-depends-on-text': {
-    validate: ['required'],
-    legend: 'fields.toggled-text.legend',
-    dependent: {
-      field: 'yes-no-radio',
-      value: 'Yes'
-    }
   }
 };
