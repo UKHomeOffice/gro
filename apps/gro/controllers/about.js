a2e9aa78c8297491259532b13a81c90bbd61142c
Brendan Butler <b.butler@kainos.com>
2015-12-07 11:40:09 +0000

    Add logic for about and details controllers

--- apps/gro/controllers/about.js
+++ apps/gro/controllers/about.js
@@ -1,6 +1,7 @@
 'use strict';
 
 var util = require('util');
+var _    = require('underscore');
 var controllers = require('hof').controllers;
 var BaseController = controllers.date;
 
@@ -13,5 +14,15 @@ util.inherits(AboutController, BaseController);
 AboutController.prototype.validateField = function validateField(keyToValidate, req) {
   return BaseController.prototype.validateField.call(this, keyToValidate, req, false);
 };
+
+AboutController.prototype.successHandler = function successHandler(req) {
+  if (_.contains(["complaint", "other"], req.form.values["about-radio"])) {
+    this.options.next = '/details';
+  } else {
+    this.options.next = '/type';
+  }
+
+  BaseController.prototype.successHandler.apply(this, arguments);
+}
 
 module.exports = AboutController;
--- apps/gro/controllers/type.js
+++ apps/gro/controllers/type.js
@@ -1,6 +1,7 @@
 'use strict';
 
 var util = require('util');
+var _    = require('underscore');
 var controllers = require('hof').controllers;
 var BaseController = controllers.base;
 
@@ -13,5 +14,23 @@ util.inherits(TypeController, BaseController);
 TypeController.prototype.validateField = function validateField(keyToValidate, req) {
   return BaseController.prototype.validateField.call(this, keyToValidate, req);
 };
+
+TypeController.prototype.getValues = function getValues(req) {
+  var previousSteps = req.sessionModel.attributes.steps;
+
+  this.options.backLink = '/gro' + previousSteps[previousSteps.length - 1];
+
+  BaseController.prototype.getValues.apply(this, arguments);
+}
+
+TypeController.prototype.successHandler = function successHandler(req) {
+  if (_.contains(["marriage", "partnership"], req.form.values["type-radio"])) {
+    this.options.next = '/people';
+  } else {
+    this.options.next = '/person';
+  }
+
+  BaseController.prototype.successHandler.apply(this, arguments);
+}
 
 module.exports = TypeController;
