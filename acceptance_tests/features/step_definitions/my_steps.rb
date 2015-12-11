9a3b454b13578912f65b1f05409788090ae1d6bb
Brendan Butler <b.butler@kainos.com>
2015-12-11 16:25:37 +0000

    Working happy path for the complaint and other journeys

--- acceptance_tests/features/step_definitions/my_steps.rb
+++ acceptance_tests/features/step_definitions/my_steps.rb
@@ -21,36 +21,36 @@ Then(/^I am taken to the (.*) page of the form$/) do | header |
 end
 
 When(/^I fill in the name on the certificate$/) do
-  step 'I enter full_name into the person-text field'
+  step 'I enter full_name into the person_text field'
   step 'I click continue'
 end
 
 When(/^I enter both names on the certificate$/) do
-  step 'I enter full_name into the person-one-text field'
-  step 'I enter first_alt_name into the person-two-text field'
+  step 'I enter full_name into the person_one field'
+  step 'I enter first_alt_name into the person_two field'
   step 'I click continue'
 end
 
 When(/^I enter my full name$/) do
-  step 'I enter full_name into the name-text field'
+  step 'I enter full_name into the name_text field'
   step 'I click continue'
 end
 
 When(/^I choose (.*) and enter my order number$/) do | field |
   find_by_id(CONTENT[field]).click
-  step 'I enter 123456 into the order-number-text field'
+  step 'I enter 123456 into the order_number field'
   step 'I click continue'
 end
 
 When(/^I enter a date$/) do
-  step 'I enter 10 into the when-date-day field'
-  step 'I enter 10 into the when-date-month field'
-  step 'I enter 2015 into the when-date-year field'
+  step 'I enter 10 into the when_day field'
+  step 'I enter 10 into the when_month field'
+  step 'I enter 2015 into the when_year field'
   step 'I click continue'
 end
 
 When(/^I choose (.*) for existing and (.*) for previous complaint$/) do | existing, previous |
-  step 'I enter free_text into the details-text field'
+  step 'I enter free_text into the details_text field'
   step "I select #{existing} on the existing-radio button"
   step "I select #{previous} on the previous-radio button"
   step 'I click continue'
@@ -61,14 +61,15 @@ When(/^I enter (.*) into the (.*) field$/) do | text, field |
 end
 
 When(/^I enter my email address$/) do
-  step 'I enter email_address into the email-text field'
+  step 'I enter email_address into the email_text field'
   step 'I click continue'
 end
 
 When(/^I fill in my address$/) do
-  step 'I enter Oz into the country-text field'
-  step 'I enter Yellow_Brick_Road into the address-text-one field'
-  step 'I enter Emerald_City into the address-text-two field'
+  step 'I enter country into the country_text field'
+  step 'I enter address_one into the address_text_one field'
+  step 'I enter address_two into the address_text_two field'
+  step 'I enter post_code into the address_text_two field'
   step 'I click continue'
 end
 
@@ -93,17 +94,6 @@ Then(/^I should see the (.*) error?/) do | type |
   expect(page).to have_content CONTENT["#{type}_error"]
 end
 
-Then(/^I am presented with validation errors for the first page$/) do
-  expect(page).to have_content 'Tell us which is your favourite superhero'
-  expect(page).to have_content 'Date must only contain numbers'
-  expect(page).to have_content 'Enter your favourite colour'
-  expect(page).to have_content 'The email address isn\'t valid, enter a valid email address'
-end
-
-Then(/^I can see another field appear$/) do
-  expect(page).to have_selector('#example-toggled-text')
-end
-
 Then(/^the hidden field disappears again$/) do
   # TODO: Reimplement this step. Currently it passes locally (with phantom or firefox) but not on Travis
   # expect(page).to_not have_selector('#example-toggled-text')
@@ -111,18 +101,6 @@ end
 
 When(/^I click Confirm submission$/) do
   click_button('Confirm submission')
-end
-
-Then(/^I am taken to the third page of the form$/) do
-  expect(page).to have_content 'This page shows how to make a field mandatory depending on the answer to a previous question'
-end
-
-Then(/^I am presented with an error for the field that is dependent on Yes being selected$/) do
-  expect(page).to have_content 'Input the name of your pet dog'
-end
-
-Then(/^I am presented with my custom validation errors$/) do
-  expect(page).to have_content 'That isn\'t a multiple of 3, try again!'
 end
 
 When(/^I click the first "Change" button$/) do
