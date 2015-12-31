e9b5d266e1b52e7dde71acc18c4874708649fd70
Joe Flintham <joeflintham@gmail.com>
2015-12-31 14:53:11 +0000

    GRO /confirm stage should utilise email service

--- apps/gro/controllers/confirm.js
+++ apps/gro/controllers/confirm.js
@@ -3,7 +3,10 @@
 var util = require('util');
 var controllers = require('hof').controllers;
 var BaseController = controllers.base;
-var _ = require('underscore');
+var path = require('path');
+var i18n = require('hof').i18n;
+
+var Model = require('../../common/models/email');
 
 var ConfirmController = function ConfirmController() {
   BaseController.apply(this, arguments);
@@ -11,13 +14,33 @@ var ConfirmController = function ConfirmController() {
 
 util.inherits(ConfirmController, BaseController);
 
+
 ConfirmController.prototype.saveValues = function saveValues(req, res, callback) {
 
-  BaseController.prototype.saveValues.call(this, req, res, function saveModel() {
-    //  You can retrieve and use data with the below call, then add further actions below to do something with the data
-      /*eslint no-unused-vars: 0*/
-      var data = _.pick(req.sessionModel.toJSON(), _.identity);
-    callback();
+  var locali18n = i18n({
+    path: path.resolve(
+      __dirname, '../translations/__lng__/__ns__.json'
+    )
+  });
+
+  locali18n.on('ready', function prepareEmail() {
+
+    var subject = locali18n.translate('pages.email-table.information.subject');
+    var model = new Model(req.sessionModel);
+    var service = {
+      template: 'gro',
+      subject: subject
+    };
+
+    if (service) {
+      model.set('template', service.template);
+      model.set('subject', service.subject);
+    } else {
+      throw new Error('no service found');
+    }
+
+    model.save(callback);
+
   });
 
 };
