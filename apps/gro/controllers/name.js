02aa0766a3be91961c14ac59eb32aafb851636fd
Joe Flintham <joeflintham@gmail.com>
2016-01-12 12:29:31 +0000

    Remove redundant code

--- apps/gro/controllers/name.js
+++ apps/gro/controllers/name.js
@@ -11,20 +11,4 @@ var NameController = function NameController() {
 
 util.inherits(NameController, BaseController);
 
-function setBackLink(req, backStep) {
-  _.each(req.sessionModel.get('steps'), function checkStep(step) {
-    if (step === '/when' || step === '/details') {
-      backStep = step.replace(/^\//, '');
-    }
-  });
-  return backStep;
-}
-
-NameController.prototype.getValues = function getValues(req, res) {
-
-  res.locals.backLink = setBackLink(req, res.locals.backLink);
-
-  BaseController.prototype.getValues.apply(this, arguments);
-};
-
 module.exports = NameController;
