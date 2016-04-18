a61398fcc9d3ef0cbf5af10f85ebb3c5dafd83bf
Geoffrey Martin <geoffreymartin@Geoffreys-MacBook-Pro-2.local>
2016-04-18 09:27:10 +0100

    implemented address form journey

---
+++ apps/gro/fields/post-inside.js
@@ -0,0 +1,17 @@
+'use strict';
+
+module.exports = {
+  'address-text-one': {
+    validate: ['required'],
+    label: 'fields.address-text-one.label'
+  },
+  'address-text-two': {
+    label: ' '
+  },
+  'address-text-three': {
+    label: ' '
+  },
+  'town': {
+    label: 'Town or city'
+  }
+};
--- assets/js/index.js
+++ assets/js/index.js
@@ -7,3 +7,52 @@ var formFocus = toolkit.formFocus;
 
 helpers.documentReady(progressiveReveal);
 helpers.documentReady(formFocus);
+
+var $ = require('jquery');
+var typeahead = require('typeahead.js-browserify');
+var Bloodhound = require('typeahead.js-browserify').Bloodhound;
+
+typeahead.loadjQueryPlugin();
+
+$('.typeahead').each(function applyTypeahead() {
+  var $el = $(this);
+  var $parent = $el.parent();
+  var attributes = $el.prop('attributes');
+  var $input = $('<input/>');
+  var selectedValue = $el.val();
+  var typeaheadList = $el.find('option').map(function mapOptions() {
+    if (this.value === '') {
+      // remove any empty values from typeahead
+      /*eslint consistent-return: 0*/
+      return;
+      /*eslint consistent-return: 1*/
+    }
+    return this.value;
+  }).get();
+
+  // remove the selectbox
+  $el.remove();
+
+  $.each(attributes, function applyAttributes() {
+    $input.attr(this.name, this.value);
+  });
+
+  $input.removeClass('js-hidden');
+  $input.addClass('form-control');
+  $input.val(selectedValue);
+
+  $parent.append($input);
+
+  $input.typeahead({
+    minLength: 1,
+    hint: false,
+    limit: 5
+  }, {
+    source: new Bloodhound({
+      datumTokenizer: Bloodhound.tokenizers.whitespace,
+      queryTokenizer: Bloodhound.tokenizers.whitespace,
+      local: typeaheadList
+    })
+  });
+});
+
