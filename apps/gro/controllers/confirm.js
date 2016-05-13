e65d56f316f7cda35e015533ef89e54aa610210f
Ben Diggle <ben.diggle@transformuk.com>
2016-05-13 15:05:14 +0100

    Changed the way the attributes in sessionModel were added to the values object to make it simplier to understand. Removed the logic around the service and setting the template and subject to the email model as it was unneeded. Changed the confirm controller to ES6

--- apps/gro/controllers/confirm.js
+++ apps/gro/controllers/confirm.js
@@ -1,63 +1,37 @@
 'use strict';
 
-var util = require('util');
-var controllers = require('hof').controllers;
-var BaseController = controllers.base;
-var path = require('path');
-var i18n = require('hof').i18n;
+const controllers = require('hof').controllers;
+const BaseController = controllers.base;
+const path = require('path');
+const i18n = require('hof').i18n;
+const Model = require('../../common/models/email');
 
-var Model = require('../../common/models/email');
+module.exports = class ConfirmController extends BaseController {
 
-var ConfirmController = function ConfirmController() {
-  BaseController.apply(this, arguments);
+  saveValues(req, res, callback) {
+    const locali18n = i18n({
+      path: path.resolve(
+        __dirname, '../translations/__lng__/__ns__.json'
+      )
+    });
+
+    locali18n.on('ready', () => {
+
+      const subject = locali18n.translate('pages.email.information.subject');
+      const data = {
+        values: Object.assign({}, req.sessionModel.toJSON(), {
+          reportDate: (new Date()).toISOString()
+        }),
+        email: req.sessionModel.get('email-text')
+      };
+
+      const model = new Model(data);
+
+      model.set('template', 'gro');
+      model.set('subject', subject);
+
+      model.save(callback);
+
+    });
+  }
 };
-
-util.inherits(ConfirmController, BaseController);
-
-
-ConfirmController.prototype.saveValues = function saveValues(req, res, callback) {
-
-  var locali18n = i18n({
-    path: path.resolve(
-      __dirname, '../translations/__lng__/__ns__.json'
-    )
-  });
-
-  locali18n.on('ready', function prepareEmail() {
-
-    var subject = locali18n.translate('pages.email.information.subject');
-
-    var d = {
-      values: (function getSessionValues(data) {
-        var r = {};
-        for (var prop in data) {
-          r[prop] = data[prop];
-        }
-        return r;
-      }(req.sessionModel.attributes))
-    };
-
-    var dateTime = new Date();
-    d.values.reportDate = dateTime.toISOString();
-    d.email = d.values['email-text'];
-
-    var model = new Model(d);
-    var service = {
-      template: 'gro',
-      subject: subject
-    };
-
-    if (service) {
-      model.set('template', service.template);
-      model.set('subject', service.subject);
-    } else {
-      throw new Error('no service found');
-    }
-
-    model.save(callback);
-
-  });
-
-};
-
-module.exports = ConfirmController;
