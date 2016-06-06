2c353f90e379aab0449324327baf98712a8088fd
Geoffrey Martin <geoffreymartin@Geoffreys-MacBook-Pro-2.local>
2016-06-06 10:31:57 +0100

    GFM-92 - Modify fields and views to use label class * address-outside fields and views. * address-inside views. * translation fields.

--- apps/gro/fields/address-outside.js
+++ apps/gro/fields/address-outside.js
@@ -3,5 +3,17 @@
 module.exports = {
   'address-text-one': {
     validate: ['required'],
+  },
+  'address-text-two': {
+    labelClassName: 'visuallyhidden'
+  },
+  'address-text-three': {
+    labelClassName: 'visuallyhidden'
+  },
+  'address-text-four': {
+    labelClassName: 'visuallyhidden'
+  },
+  'address-text-five': {
+    labelClassName: 'visuallyhidden'
   }
 };
