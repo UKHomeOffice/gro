8b8363deec6681cb1d4e33d7f14ee783b10d53ca
joefitter <hello@joefitter.com>
2016-09-22 15:37:07 +0100

    fix for address lookup
    * use objects for options to stop implicit lookup

--- apps/gro/controllers/address-lookup.js
+++ apps/gro/controllers/address-lookup.js
@@ -7,10 +7,16 @@ module.exports = class AddressLookup extends BaseController {
 
   getValues(req, res, callback) {
     const addresses = req.sessionModel.get('addresses');
-    const formattedlist = _.map(_.map(addresses, 'formatted_address'), arg => arg.split('\n').join(', '));
+    const formattedlist = _.map(_.map(addresses, 'formatted_address'), address => {
+      address = address.split('\n').join(', ');
+      return {
+        value: address,
+        label: address
+      };
+    });
 
     const count = `${formattedlist.length} addresses`;
-    this.options.fields['address-lookup'].options = [count].concat(formattedlist);
+    this.options.fields['address-lookup'].options = [{value: count, label: count}].concat(formattedlist);
     super.getValues(req, res, callback);
   }
 
@@ -22,7 +28,7 @@ module.exports = class AddressLookup extends BaseController {
   }
 
   validateField(key, req) {
-    if (req.form.values[key] === this.options.fields['address-lookup'].options[0]) {
+    if (req.form.values[key] === this.options.fields['address-lookup'].options[0].value) {
       return new ErrorController('address-lookup', {
         key: 'address-lookup',
         type: 'required',
