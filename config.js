8cdc5da7df75cf43fbb6ac8c90951ab79574f059
Ben Marvell <madmarv187@gmail.com>
2018-07-20 12:23:52 +0100

    use the passed in redis port and host

--- config.js
+++ config.js
@@ -6,6 +6,8 @@ process.title = 'gro';
 module.exports = {
   env: process.env.NODE_ENV,
   redis: {
+    port: process.env.REDIS_PORT || 6379,
+    host: process.env.REDIS_HOST || '127.0.0.1',
     password: process.env.REDIS_PASSWORD
   },
   email: {
