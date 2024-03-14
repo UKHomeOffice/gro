690684b1d8d641e99e985db00f6e866cfee2e5d6
Paola Maddaluno <83777310+PaolaDMadd-Pro@users.noreply.github.com>
2024-03-14 17:15:01 +0000

    [GRO-166] file rename

--- apps/gro/behaviours/reverseFieldValue.js
+++
@@ -1,6 +0,0 @@
-module.exports = config => superclass => class extends superclass {
-    saveValues(req, res, next) {
-      req.form.values[`${config.key}-reversed`] = reverse(req.form.values[config.key]);
-      super.saveValues(req, res, next);
-    }
-  };
