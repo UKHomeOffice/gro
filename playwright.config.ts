c836a776d1985d8c449363917b0bb56cbcb912b7
GrahamOsifoHO <Graham.Osifo@digital.homeoffice.gov.uk>
2026-07-15 15:07:08 +0100

    update playwright config

--- playwright.config.ts
+++ playwright.config.ts
@@ -4,7 +4,9 @@ import dotenv from 'dotenv';
 
 dotenv.config({ quiet: true });
 
-const baseURL = process.env.PLAYWRIGHT_BASE_URL || `https://certificate-enquiries.uat.sas-notprod.homeoffice.gov.uk/`;
+//Note: The baseURL is set to the value of PLAYWRIGHT_BASE_URL from the .env file if it exists, otherwise it defaults to http://localhost:${port}.
+const port = Number(process.env.PLAYWRIGHT_PORT || 8080);
+const baseURL = process.env.PLAYWRIGHT_BASE_URL || `http://localhost:${port}`;
 
 const testDir = defineBddConfig({
     features: 'e2e-tests/features/**/*.feature',
@@ -34,7 +36,14 @@ export default defineConfig({
         trace: 'on-first-retry',
         headless: !!process.env.CI,
     },
-
+    webServer: process.env.PLAYWRIGHT_BASE_URL
+    ? undefined
+    : {
+      command: 'yarn start:dev',
+      port,
+      reuseExistingServer: !process.env.CI,
+      timeout: 120_000
+    },
     /* Configure projects for major browsers */
     projects: [
         {
